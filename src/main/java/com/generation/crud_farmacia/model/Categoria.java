5b345f3bb3e621107f22fbf8dc3495ed3db86090
Ricardo S. Ferreira <ricardo.silva_ferreira@hotmail.com>
2024-07-22 09:52:05 -0300

    Configurando o CRUD Categoria

---
+++ src/main/java/com/generation/crud_farmacia/model/Categoria.java
@@ -0,0 +1,52 @@
+package com.generation.crud_farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@NotBlank(message = "nome obrigatório")
+	@Size(min = 5, max = 100, message = "O nome deve conter no mínimo 5 e no máximo 100 caracters")
+	private String nome;
+	
+	@NotBlank(message = "descrição obrigatória")
+	@Size(min = 5, max = 600, message = "A descrição deve conter no mínimo 5 e no máximo 600 caracters")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+    
+	
+}
